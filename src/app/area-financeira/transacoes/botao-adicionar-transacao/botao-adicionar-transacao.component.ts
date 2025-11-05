9c9ff567cc3b1c591ec4bcdd0d25098e3d42b09d
Alexandre Costa <alexandre.costa@furg.br>
2025-11-05 17:53:33 -0300

    feat: refactor Transacao model to handle date parsing and update transaction button component to log new transactions

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 import { FormsModule } from "@angular/forms";
+import { TipoTransacao, Transacao } from '../../compartilhados/transacao.model';
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
@@ -25,6 +26,14 @@ export class BotaoAdicionarTransacaoComponent {
   }
 
   aoSubmeter() {
-    console.log(this.novaTransacaoForm);
+    const novaTransacao = new Transacao(
+      this.novaTransacaoForm.nome,
+      this.novaTransacaoForm.tipo as TipoTransacao,
+      Number(this.novaTransacaoForm.valor),
+      this.novaTransacaoForm.data,
+      this.novaTransacaoForm.conta
+    );
+
+    console.log('Nova transação adicionada:', novaTransacao);
   }
 }
