af852a3e0f66209bdb5ef572d7850fb09858b97b
Alexandre Costa <alexandre.costa@furg.br>
2025-11-05 18:26:45 -0300

    feat: add account creation functionality with modal and form handling in BotaoAdicionarConta component

--- src/app/area-financeira/contas/contas.component.ts
+++ src/app/area-financeira/contas/contas.component.ts
@@ -1,14 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { BotaoComponent } from "../../compartilhados/botao/botao.component";
 import { ContaComponent } from "./conta/conta.component";
 import { Conta } from '../compartilhados/conta.model';
+import { BotaoAdicionarContaComponent } from "./botao-adicionar-conta/botao-adicionar-conta.component";
 
 @Component({
   selector: 'app-contas',
-  imports: [BotaoComponent, ContaComponent],
+  imports: [ContaComponent, BotaoAdicionarContaComponent],
   templateUrl: './contas.component.html',
   styleUrl: './contas.component.css'
 })
 export class ContasComponent {
   contas = input.required<Conta[]>();
+
+  contaCriada = output<Conta>();
 }
